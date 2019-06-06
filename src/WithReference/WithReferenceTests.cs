55c63559ee3c7e7475eb8be95689f3d354a9a17a
david.schruf <david.schruf@wikifolio.com>
2019-06-06 13:17:32 +0200

    issue with references

---
+++ src/WithReference/WithReferenceTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace WithReference
+{
+    [TestFixture]
+    public class WithReferenceTests
+    {
+        [Test]
+        public void Fails()
+        {
+            Assert.That(() => new NLog.Extensions.Logging.NLogLoggerFactory(), Throws.Nothing);
+        }
+    }
+}
---
+++ src/WithoutReference/WithoutReferenceTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace WithoutReference
+{
+    [TestFixture]
+    public class WithoutReferenceTests
+    {
+        [Test]
+        public void Succeeds()
+        {
+            Assert.That(() => new NLog.Extensions.Logging.NLogLoggerFactory(), Throws.Nothing);
+        }
+    }
+}
